5291732c2b1c10d0aee7a367f218b7a0a553f38d
Antoine Cotten <hello@acotten.com>
2021-07-29 13:53:57 +0200

    Add startupProbe to event_display Deployment
    Eliminates risks of sending events before the service is up and running.

--- test/e2e/framework/bridges/components.go
+++ test/e2e/framework/bridges/components.go
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2020 TriggerMesh Inc.
+Copyright (c) 2021 TriggerMesh Inc.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -41,7 +41,6 @@ func CreateBridge(brdgCli dynamic.ResourceInterface, bridge *unstructured.Unstru
 
 	return brdgCli.Create(context.Background(), bridge, metav1.CreateOptions{})
 }
-
 
 // Components returns the Bridge's components as a list of
 // map[string]interface{}.
